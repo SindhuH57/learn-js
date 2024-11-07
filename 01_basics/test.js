73ab148cfc4af0aa255c62aa3647bc4f13a42d8c
SindhuH57 <107095265+SindhuH57@users.noreply.github.com>
2024-11-07 13:17:06 +0000

    basics configs

---
+++ 01_basics/test.js
@@ -0,0 +1,10 @@
+const accountId = 12432
+let aname = "Sindhu"
+var city = "Bijapur"
+state = "karnataka"
+
+let country;
+
+// dont use var because of issue with block scope and functional scope
+
+console.table([accountId, aname, city, state, country])
